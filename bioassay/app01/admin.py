1d30633303e35fa812f7fe4a95181d9f09089d57
spx88 <47275222+spx88@users.noreply.github.com>
2020-10-11 17:30:50 +0800

    Add files via upload

---
+++ bioassay/app01/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+# from app01.models import Publisher, Book, Country, Author
+#
+# # Register your models here.
+# admin.site.register()
+# admin.site.register(Book)
+# admin.site.register(Country)
+# admin.site.register(Author)
